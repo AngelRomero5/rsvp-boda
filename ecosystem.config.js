8d122a3c2e6945e4ec60d00b05a4f243a2633a61
angelromero <angel.romero@fwpr.com>
2026-02-02 13:57:32 -0400

    Added env file with credentials for mega

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
     apps: [{
         name: "rsvp-app",
         script: "./server.js",
+        env_file: "./server-config.env",
         instances: 1,
         exec_mode: "cluster",
         env: {
